82af360950d01320ee722350aed828cc718f3623
Carolina Cuadra Pardo <74510425+cuadrapardo@users.noreply.github.com>
2024-06-12 17:21:23 +0100

    Change data structure to include color

--- render/cw1/point_cloud.cpp
+++ render/cw1/point_cloud.cpp
@@ -4,16 +4,17 @@
 
 #include "point_cloud.hpp"
 #include <unordered_set>
+#include <algorithm>
 
 PointCloud obj_to_pointcloud(SimpleModel& obj_file) {
     PointCloud pCloud;
-    for (const auto& point : obj_file.dataUntextured.positions) {
-        pCloud.points.insert(point);
-    }
+    //Add points
+    pCloud.points.insert(pCloud.points.end(), obj_file.dataTextured.positions.begin(),  obj_file.dataTextured.positions.end());
+    pCloud.points.insert(pCloud.points.end(), obj_file.dataUntextured.positions.begin(),  obj_file.dataUntextured.positions.end());
 
-    for (const auto& point : obj_file.dataTextured.positions) {
-        pCloud.points.insert(point);
-    }
+    //Add color (default to red)
+    pCloud.colors.resize(pCloud.points.size());
+    std::fill(pCloud.colors.begin(), pCloud.colors.end(), glm::vec3{1.0f, 0.0f, 0.0f});
 
     return pCloud;
 }
--- render/cw1/point_cloud.hpp
+++ render/cw1/point_cloud.hpp
@@ -8,23 +8,11 @@
 #include <unordered_set>
 #include "simple_model.hpp"
 
-template <>
-struct std::hash<glm::vec3> {
-    std::size_t operator()(const glm::vec3& v) const {
-        std::size_t h1 = std::hash<float>()(v.x);
-        std::size_t h2 = std::hash<float>()(v.y);
-        std::size_t h3 = std::hash<float>()(v.z);
-        return h1 ^ (h2 << 1) ^ (h3 << 2);
-    }
-};
-
-// Equality operator for glm::vec3
-bool operator==(const glm::vec3& lhs, const glm::vec3& rhs) {
-    return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z;
-}
 
 struct PointCloud {
-    std::unordered_set<glm::vec3> points;
+    std::vector<glm::vec3> points;
+    std::vector<glm::vec3> colors;
+
 };
 
 PointCloud obj_to_pointcloud(SimpleModel& obj_file); //TODO: do I even need this function?
